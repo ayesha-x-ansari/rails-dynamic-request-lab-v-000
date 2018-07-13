b9cb3f25f65033847163b63d2f8e891c7e43e515
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-07-13 14:42:57 +0000

    automatically backed up by learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Student.create(first_name: "ayesha, last_name: "ansari")
+Student.create(first_name: "ayesha", last_name: "ansari")
