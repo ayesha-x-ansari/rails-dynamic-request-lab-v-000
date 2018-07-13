5056c06e034be10ef2f5da56e71c75765bd82ad4
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-07-13 14:35:50 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,6 +5,11 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def ddd
+    <h1> dddddddddd</h1>
+  end
+
+
   def show
     @student = Student.find(params[:id])
   end
